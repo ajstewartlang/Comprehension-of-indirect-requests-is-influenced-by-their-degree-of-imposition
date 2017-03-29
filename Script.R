7b3b3b175e7431b6b445a269e5349aaeed290eb8
ajstewartlang <andrewstewart@Andrews-MacBook-Air.local>
2017-03-29 15:28:45 +0100

    Made consistent the labelling of factors in data files and in paper

--- Script.R
+++ Script.R
@@ -11,110 +11,117 @@ library (pbkrtest)
 #Read in First Pass Data
 FPs <- read.csv("~/FPs.csv")
 
+FPs$Meaning <- as.factor(FPs$Meaning)
+FPs$Imposition <- as.factor(FPs$Imposition)
+
 #this sets up the contrasts so that the intercept in the mixed LMM is the grand mean (i.e., the mean of all conditions)
 my.coding <- matrix (c(.5, -.5))
-contrasts (FPs$Meaning)<-my.coding  
-contrasts (FPs$Probability)<-my.coding 
+
+contrasts (FPs$Meaning) <- my.coding  
+contrasts (FPs$Imposition) <- my.coding 
 
 #construct the models with crossed random effects for subjects and items for the pre-critical, critical and post-crtical regions of text on first pass times
-fpmodelprec <- lmer (Speaker ~ Meaning*Probability + (1+Meaning*Probability |P.s) + (1+Meaning+Probability |Item), data=FPs, REML=TRUE)
+fpmodelprec <- lmer (Probmanip ~ Meaning*Imposition + (1+Meaning*Imposition |P.s) + (1+Meaning+Imposition |Item), data=FPs, REML=TRUE)
 summary (fpmodelprec)
-lsmeans (fpmodelprec, pairwise~Meaning*Probability, adjust="none")
+lsmeans (fpmodelprec, pairwise~Meaning*Imposition, adjust="none")
 
-fpmodelc <- lmer (statement ~ Meaning*Probability + (1+Meaning*Probability |P.s) + (1+Meaning*Probability |Item), data=FPs, REML=TRUE)
+fpmodelc <- lmer (statement ~ Meaning*Imposition + (1+Meaning*Imposition |P.s) + (1+Meaning*Imposition |Item), data=FPs, REML=TRUE)
 summary (fpmodelc)
-lsmeans (fpmodelc, pairwise~Meaning*Probability, adjust="none")
+lsmeans (fpmodelc, pairwise~Meaning*Imposition, adjust="none")
 
-fpmodelpostc <- lmer (response ~ Meaning*Probability + (1+Meaning*Probability |P.s) + (1+Meaning+Probability |Item), data=FPs, REML=TRUE)
+fpmodelpostc <- lmer (response ~ Meaning*Imposition + (1+Meaning*Imposition |P.s) + (1+Meaning+Imposition |Item), data=FPs, REML=TRUE)
 summary (fpmodelpostc)
-lsmeans (fpmodelpostc, pairwise~Meaning*Probability, adjust="none")
+lsmeans (fpmodelpostc, pairwise~Meaning*Imposition, adjust="none")
 
 
 #Read in Regression Path data
 RPs <- read.csv("~/RPs.csv")
 
-contrasts (RPs$Meaning)<-my.coding  
-contrasts (RPs$Probability)<-my.coding 
+RPs$Meaning <- as.factor(RPs$Meaning)
+RPs$Imposition <- as.factor(RPs$Imposition)
+
+contrasts (RPs$Meaning) <- my.coding  
+contrasts (RPs$Imposition) <- my.coding 
 
 #construct the models with crossed random effects for subjects and items for the pre-critical, critical and post-crtical regions of text on regression path times
-rpmodelprec <- lmer (Speaker ~ Meaning*Probability + (1+Meaning*Probability |P.s) + (1+Meaning*Probability |Item), data=RPs, REML=TRUE)
+rpmodelprec <- lmer (Probmanip ~ Meaning*Imposition + (1+Meaning*Imposition |P.s) + (1+Meaning*Imposition |Item), data=RPs, REML=TRUE)
 summary (rpmodelprec)
-lsmeans (rpmodelprec, pairwise~Meaning*Probability, adjust="none")
+lsmeans (rpmodelprec, pairwise~Meaning*Imposition, adjust="none")
 
-rpmodelc <- lmer (statement ~ Meaning*Probability + (1+Meaning*Probability |P.s) + (1+Meaning*Probability |Item), data=RPs, REML=TRUE)
+rpmodelc <- lmer (statement ~ Meaning*Imposition + (1+Meaning*Imposition |P.s) + (1+Meaning*Imposition |Item), data=RPs, REML=TRUE)
 summary (rpmodelc)
-lsmeans (rpmodelc, pairwise~Meaning*Probability, adjust="none")
+lsmeans (rpmodelc, pairwise~Meaning*Imposition, adjust="none")
 
-rpmodelpostc <- lmer (response ~ Meaning*Probability + (1+Meaning*Probability |P.s) + (1+Meaning*Probability |Item), data=RPs, REML=TRUE)
+rpmodelpostc <- lmer (response ~ Meaning*Imposition + (1+Meaning*Imposition |P.s) + (1+Meaning*Imposition |Item), data=RPs, REML=TRUE)
 summary (rpmodelpostc)
-lsmeans (rpmodelpostc, pairwise~Meaning*Probability, adjust="none")
+lsmeans (rpmodelpostc, pairwise~Meaning*Imposition, adjust="none")
 
 #Read in total reading time data
-TTs <- read.csv("~/TTsb.csv")
+TTs <- read.csv("~/TTs.csv")
 
-contrasts (TTs$Meaning)<-my.coding  
-contrasts (TTs$Probability)<-my.coding 
+TTs$Meaning <- as.factor(TTs$Meaning)
+TTs$Imposition <- as.factor(TTs$Imposition)
+
+contrasts (TTs$Meaning) <- my.coding  
+contrasts (TTs$Imposition) <- my.coding 
 
 #construct the models with crossed random effects for subjects and items for the pre-critical, critical and post-crtical regions of text on total reading times
-ttmodelprec <- lmer (Speaker ~ Meaning*Probability + (1+Meaning*Probability |P.s) + (1+Meaning+Probability |Item), data=TTs, REML=TRUE)
+ttmodelprec <- lmer (Probmanip ~ Meaning*Imposition + (1+Meaning*Imposition |P.s) + (1+Meaning+Imposition |Item), data=TTs, REML=TRUE)
 summary (ttmodelprec)
-lsmeans (ttmodelprec, pairwise~Meaning*Probability, adjust="none")
+lsmeans (ttmodelprec, pairwise~Meaning*Imposition, adjust="none")
 
-ttmodelc <- lmer (statement ~ Meaning*Probability + (1+Meaning*Probability |P.s) + (1+Meaning*Probability |Item), data=TTs, REML=TRUE)
+ttmodelc <- lmer (statement ~ Meaning*Imposition + (1+Meaning*Imposition |P.s) + (1+Meaning*Imposition |Item), data=TTs, REML=TRUE)
 summary (ttmodelc)
-lsmeans (ttmodelc, pairwise~Meaning*Probability, adjust="none")
+lsmeans (ttmodelc, pairwise~Meaning*Imposition, adjust="none")
 
-ttmodelpostc <- lmer (response ~ Meaning*Probability + (1+Meaning*Probability |P.s) + (1+Meaning*Probability |Item), data=TTs, REML=TRUE)
+ttmodelpostc <- lmer (response ~ Meaning*Imposition + (1+Meaning*Imposition |P.s) + (1+Meaning*Imposition |Item), data=TTs, REML=TRUE)
 summary (ttmodelpostc)
-lsmeans (ttmodelpostc, pairwise~Meaning*Probability, adjust="none")
+lsmeans (ttmodelpostc, pairwise~Meaning*Imposition, adjust="none")
 
 #Read in regressions out data
 ROs <- read.csv("~/ROs.csv")
 
-contrasts (ROs$Meaning)<-my.coding  
-contrasts (ROs$Probability)<-my.coding 
+ROs$Meaning <- as.factor(ROs$Meaning)
+ROs$Imposition <- as.factor(ROs$Imposition)
+
+contrasts (ROs$Meaning) <- my.coding  
+contrasts (ROs$Imposition) <- my.coding 
 
 #run separate F1 and F2 models as model with both random effects included fails to converge 
-romodelprecf1 <- glmer(Speaker ~ Meaning*Probability + (1+Meaning |P.s) , data = ROs, family=binomial) 
+romodelprecf1 <- glmer(Probmanip ~ Meaning*Imposition + (1+Meaning |P.s) , data = ROs, family=binomial) 
 summary (romodelprecf1)
 
-romodelprecf2 <- glmer(Speaker ~ Meaning*Probability + (1+Meaning|Item), data = ROs, family=binomial) 
+romodelprecf2 <- glmer(Probmanip ~ Meaning*Imposition + (1+Meaning|Item), data = ROs, family=binomial) 
 summary (romodelprecf2)
 
 #run separate F1 and F2 models as model with both random effects included fails to converge 
-romodelcf1 <- glmer(statement ~ Meaning*Probability + (1+Meaning|P.s), data = ROs, family=binomial) 
+romodelcf1 <- glmer(statement ~ Meaning*Imposition + (1+Meaning|P.s), data = ROs, family=binomial) 
 summary (romodelcf1)
 
-romodelcf2 <- glmer(statement ~ Meaning*Probability + (1+Meaning |Item), data = ROs, family=binomial) 
+romodelcf2 <- glmer(statement ~ Meaning*Imposition + (1+Meaning |Item), data = ROs, family=binomial) 
 summary (romodelcf2)
 
 #run separate F1 and F2 models as model with both random effects included fails to converge 
-romodelpostcf1 <- glmer(response ~ Meaning*Probability + (1+Meaning |P.s) , data = ROs, family=binomial) 
+romodelpostcf1 <- glmer(response ~ Meaning*Imposition + (1+Meaning |P.s) , data = ROs, family=binomial) 
 summary (romodelpostcf1)
-lsmeans (romodelpostcf1, pairwise~Meaning*Probability, adjust="none")
+lsmeans (romodelpostcf1, pairwise~Meaning*Imposition, adjust="none")
 
-romodelpostcf2 <- glmer(response ~ Meaning*Probability + (1+Meaning|Item) , data = ROs, family=binomial) 
+romodelpostcf2 <- glmer(response ~ Meaning*Imposition + (1+Meaning|Item) , data = ROs, family=binomial) 
 summary (romodelpostcf2)
 
 #Read in regressions in data
 RIs <- read_csv("~/RIs.csv")
 
 RIs$Meaning <- as.factor(RIs$Meaning)
-RIs$Probability <- as.factor (RIs$Probability)
+RIs$Imposition <- as.factor (RIs$Imposition)
 
-contrasts (RIs$Meaning)<-my.coding  
-contrasts (RIs$Probability)<-my.coding 
+contrasts (RIs$Meaning) <- my.coding  
+contrasts (RIs$Imposition) <- my.coding 
 
 #run separate F1 and F2 models as model with both random effects included fails to converge 
 
-rimodelimpf1 <- glmer(Probmanip ~ Meaning*Probability + (1+Meaning+Probability |P.s) , data = RIs, family=binomial) 
+rimodelimpf1 <- glmer(Probmanip ~ Meaning*Imposition + (1+Meaning|P.s) , data = RIs, family=binomial) 
 summary (rimodelimpf1)
 
-rimodelimpf2 <- glmer(Probmanip ~ Meaning*Probability + (1+Meaning+Probability |Item) , data = RIs, family=binomial) 
+rimodelimpf2 <- glmer(Probmanip ~ Meaning*Imposition + (1+Meaning+Imposition |Item) , data = RIs, family=binomial) 
 summary (rimodelimpf2)
-
-
-
-
-
-
